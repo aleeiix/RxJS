9c4c96bc6a2f8e0da3bce5d7902fec31e44b52e4
Aleix Riba <aleixriba33@gmail.com>
2019-08-31 14:32:49 +0200

    finish class

--- src/create.js
+++ src/create.js
@@ -1,9 +1,13 @@
 import { displayLog } from './utils';
+import { Observable } from 'rxjs';
 
 export default () => {
-    /** start coding */
+    const hello = Observable.create(function(observer){
+        observer.next('Hello');
+        setTimeout(() => {
+            observer.next('World');
+        }, 2000);  
+    })
 
-
-
-    /** end coding */
+    const subscribe = hello.subscribe(evt => displayLog(evt));
 }
