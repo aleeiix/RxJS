1452c20cf1546f713d74af01973c45f7564e651f
Aleix Riba <aleix.riba@cabsa.es>
2019-11-05 20:06:37 +0100

    operator of

--- src/create.js
+++ src/create.js
@@ -1,6 +1,15 @@
 import { displayLog } from './utils';
-import { from } from 'rxjs';
+import { of } from 'rxjs';
 
 export default () => {
-    
+    const source = of(1, 2, 3, 4, 5, 6);
+    const source2 = of(
+        [1, 2, 3],
+        'Hello World',
+        {foo: 'bar'},
+        function sayHello() {
+            return 'Hi';
+        }
+    );
+    const subscription = source2.subscribe(res => displayLog(res));
 }
