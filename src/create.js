1b31df99ddceb6757fabe346e909966dcf2e5bef
Aleix Riba <aleix.riba@cabsa.es>
2019-11-05 19:55:22 +0100

    finish class

--- src/create.js
+++ src/create.js
@@ -2,21 +2,5 @@ import { displayLog } from './utils';
 import { Observable } from 'rxjs';
 
 export default () => {
-    const hello = Observable.create(function(observer){
-        observer.next('Hello');
-        setTimeout(() => {
-            observer.next('World');
-            observer.complete();
-        }, 2000);
-    })
-
-    const oberser = {
-        next: evt => displayLog(evt),
-        error: err => console.error('[ERROR] - ', err),
-        complete: () => displayLog('[DONE]')
-    }
-
-    const subscribe = hello.subscribe(oberser);
-    const subscribe2 = hello.subscribe(oberser);
-    subscribe.unsubscribe();
+    const myArray = [1, 2, 3, 4, 5]; 
 }
