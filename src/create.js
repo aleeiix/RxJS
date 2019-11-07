7f6cb6122805f66e7dfde8d5c0a87811b06562a5
Aleix Riba <aleix.riba@cabsa.es>
2019-11-07 19:42:20 +0100

    finish class

--- src/create.js
+++ src/create.js
@@ -1,6 +1,12 @@
 import { displayLog } from './utils';
-import { interval, timer } from 'rxjs';
+import { fromEvent} from 'rxjs';
 
 export default () => {
-    
+    const actionBtn = document.getElementById('action-btn');
+    const source = fromEvent(actionBtn, 'click');
+    source.subscribe((evt) => {
+        displayLog(`Click event at pos (${evt.x}, ${evt.y})`)
+    })
+
+    fromEvent(document, 'mousemove').subscribe((evt) => console.log(evt))
 }
