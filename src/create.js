297ab5420861bcada33d4ed8e46ea5a60845df36
Aleix Riba <aleix.riba@cabsa.es>
2019-11-07 19:20:02 +0100

    start class

--- src/create.js
+++ src/create.js
@@ -2,11 +2,5 @@ import { displayLog } from './utils';
 import { interval, timer } from 'rxjs';
 
 export default () => {
-    const source = interval(500);
-    const subscription = source.subscribe((data) => displayLog(data));
-    timer(3000).subscribe(() => subscription.unsubscribe());
-
-    const source2 = timer(4000, 100);
-    const subscription2 = source2.subscription((data) => displayLog(`2 - ${data}`));
-    timer(2000).subscribe(() => subscription2.unsubscribe());
+    
 }
