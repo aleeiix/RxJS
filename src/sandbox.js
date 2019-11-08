6265eb15a7bd607f0063cefff940e1e588ebf135
Aleix Riba <aleix.riba@cabsa.es>
2019-11-08 20:44:02 +0100

    finish chass

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,13 +1,15 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { mapTo, map, filter } from 'rxjs/operators'
+import { mapTo, map, filter, tap } from 'rxjs/operators'
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
     const subscription = fromEvent(grid, 'click')
     .pipe(
+        tap((evt) => console.log('Before: ', evt)),
         map((evt) => [Math.floor(evt.offsetX / 50), Math.floor(evt.offsetY / 50)]),
+        tap((evt) => console.log('After: ', evt)),
         filter((val) => (val[0] + val[1]) % 2 !== 0)
     ).subscribe((evt) => console.log(evt));
 
