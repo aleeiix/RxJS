a28844d09186bb119d8bf5325ef73135ae9068af
Aleix Riba <aleix.riba@cabsa.es>
2019-11-11 18:37:40 +0100

    start class

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,17 +1,14 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { mapTo, map, filter, tap } from 'rxjs/operators'
+import { map } from 'rxjs/operators'
 
 export default () => {
     /** start coding */
     const grid = document.getElementById('grid');
     const subscription = fromEvent(grid, 'click')
     .pipe(
-        tap((evt) => console.log('Before: ', evt)),
         map((evt) => [Math.floor(evt.offsetX / 50), Math.floor(evt.offsetY / 50)]),
-        tap((evt) => console.log('After: ', evt)),
-        filter((val) => (val[0] + val[1]) % 2 !== 0)
-    ).subscribe((evt) => console.log(evt));
+    ).subscribe((evt) => displayLog(evt));
 
     /** end coding */
 }
