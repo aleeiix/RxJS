5cdea7e42b96732932e670c4d6205f2b553a5fc0
Aleix Riba <aleix.riba@cabsa.es>
2019-11-11 19:46:52 +0100

    finish class

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, first, take, takeWhile, last, takeLast, skip } from 'rxjs/operators'
+import { map, first, take, takeWhile, last, takeLast, skip, reduce, scan } from 'rxjs/operators'
 
 export default () => {
     /** start coding */
@@ -8,8 +8,14 @@ export default () => {
     const subscription = fromEvent(grid, 'click')
     .pipe(
         map((evt) => [Math.floor(evt.offsetX / 50), Math.floor(evt.offsetY / 50)]),
-        skip(5)
-    ).subscribe((evt) => displayLog(evt));
+        takeWhile(([col, row]) => col !== 0),
+        scan((accumulated, current) => {
+            return {
+                clicks: accumulated.clicks + 1,
+                cells: [...accumulated.cells, current]
+            }
+        }, {clicks: 0, cells: []})
+    ).subscribe((evt) => displayLog(`clicks: ${evt.clicks}, cells: ${JSON.stringify(evt.cells)}`));
 
     /** end coding */
 }
