b48bb43ef6f6ab61d9cca28a193fc17c6413b999
Aleix Riba <aleix.riba@cabsa.es>
2019-11-14 18:57:32 +0100

    finish class

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, first, take, takeWhile, last, takeLast, skip, reduce, scan, tap, startWith, endWith } from 'rxjs/operators'
+import { map, first, take, takeWhile, last, takeLast, skip, reduce, scan, tap, startWith, endWith, distinct, distinctUntilChanged } from 'rxjs/operators'
 
 export default () => {
     /** start coding */
@@ -10,8 +10,7 @@ export default () => {
         map((evt) => [Math.floor(evt.offsetX / 50), Math.floor(evt.offsetY / 50)]),
         takeWhile(([col, row]) => col !== 0),
         tap((val) => console.log(val)),
-        startWith('Hola', 'Adios'),
-        endWith('Adios', 'A tu casa')
+        distinctUntilChanged((cel1, cel2) => (cel1[0] === cel2[0]) && (cel1[1] === cel2[1]))
     ).subscribe((data) => displayLog(data));
 
     /** end coding */
