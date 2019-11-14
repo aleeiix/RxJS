ac3111e81a1140cf10eb783572800ce8343a899f
Aleix Riba <aleix.riba@cabsa.es>
2019-11-14 19:03:03 +0100

    start class

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,17 +1,31 @@
-import { displayLog } from './utils';
+import { updateDisplay } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, first, take, takeWhile, last, takeLast, skip, reduce, scan, tap, startWith, endWith, distinct, distinctUntilChanged } from 'rxjs/operators'
+import { map, tap } from 'rxjs/operators';
 
 export default () => {
     /** start coding */
-    const grid = document.getElementById('grid');
-    const subscription = fromEvent(grid, 'click')
-    .pipe(
-        map((evt) => [Math.floor(evt.offsetX / 50), Math.floor(evt.offsetY / 50)]),
-        takeWhile(([col, row]) => col !== 0),
-        tap((val) => console.log(val)),
-        distinctUntilChanged((cel1, cel2) => (cel1[0] === cel2[0]) && (cel1[1] === cel2[1]))
-    ).subscribe((data) => displayLog(data));
+    const progressBar = document.getElementById('progress-bar');
+    const docElement = document.documentElement;
+    const updateProgressBar = (percentage) => {
+        progressBar.style.width = `${percentage}%`;
+    }
+
+    //observable that returns scroll (from top) on scroll events
+    const scroll$ = fromEvent(document, 'scroll').pipe(
+        map(() => docElement.scrollTop),
+        tap(evt => console.log("[scroll]: ", evt))
+    );
+
+    //observable that returns the amount of page scroll progress
+    const scrollProgress$ = scroll$.pipe(
+        map(evt => {
+            const docHeight = docElement.scrollHeight - docElement.clientHeight;
+            return (evt / docHeight) * 100;
+        })
+    )
+
+    //subscribe to scroll progress to paint a progress bar
+    const subscription = scrollProgress$.subscribe(updateProgressBar);
 
     /** end coding */
 }
