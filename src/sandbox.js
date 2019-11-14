4de2ac5e73a8c9070dae04c8731e16d569d5b9bd
Aleix Riba <aleix.riba@cabsa.es>
2019-11-14 18:24:44 +0100

    finish class

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { displayLog } from './utils';
 import { fromEvent } from 'rxjs';
-import { map, first, take, takeWhile, last, takeLast, skip, reduce, scan, tap } from 'rxjs/operators'
+import { map, first, take, takeWhile, last, takeLast, skip, reduce, scan, tap, startWith, endWith } from 'rxjs/operators'
 
 export default () => {
     /** start coding */
@@ -9,7 +9,9 @@ export default () => {
     .pipe(
         map((evt) => [Math.floor(evt.offsetX / 50), Math.floor(evt.offsetY / 50)]),
         takeWhile(([col, row]) => col !== 0),
-        tap((val) => console.log(val))
+        tap((val) => console.log(val)),
+        startWith('Hola', 'Adios'),
+        endWith('Adios', 'A tu casa')
     ).subscribe((data) => displayLog(data));
 
     /** end coding */
