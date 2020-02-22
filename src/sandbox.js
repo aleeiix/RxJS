f4888535ae5c41e45ba3967e75feda09bf265d6b
Aleix Riba <aleix.riba@cabsa.es>
2020-02-22 17:47:00 +0100

    finish class

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,6 +1,6 @@
 import { updateDisplay, displayLog } from "./utils";
 import { api } from "./api";
-import { merge, fromEvent } from "rxjs";
+import { merge, fromEvent, concat, forkJoin } from "rxjs";
 import { map, endWith } from "rxjs/operators";
 
 export default () => {
@@ -17,11 +17,17 @@ export default () => {
     const comment4$ = api.getComment(4);
 
     //subscribe to all the observables to get and display comments
-    merge(comment1$, comment2$, comment3$, comment4$)
-      .pipe(
-        map(({ id, comment }) => `#${id} - ${comment}`),
-        endWith("--------//--------")
-      )
+    // concat(comment1$, comment2$, comment3$, comment4$)
+    //   .pipe(
+    //     map(({ id, comment }) => `#${id} - ${comment}`),
+    //     endWith("--------//--------")
+    //   )
+    //   .subscribe(data => {
+    //     displayLog(data);
+    //   });
+
+    forkJoin(comment1$, comment2$, comment3$, comment4$)
+      .pipe(map(JSON.stringify), endWith("--------//--------"))
       .subscribe(data => {
         displayLog(data);
       });
