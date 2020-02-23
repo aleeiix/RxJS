3362caee1e7ea8cf4a958b497dd53f41cfc02610
Aleix Riba <aleix.riba@cabsa.es>
2020-02-23 18:18:08 +0100

    finish class

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,7 +1,7 @@
 import { updateDisplay, displayLog } from "./utils";
 import { api } from "./api";
 import { fromEvent } from "rxjs";
-import { map, scan, tap, concatMap } from "rxjs/operators";
+import { map, scan, tap, concatMap, catchError, retry } from "rxjs/operators";
 
 export default () => {
   /** start coding */
@@ -9,14 +9,30 @@ export default () => {
   const button = document.getElementById("btn");
 
   /** get comments on button click */
+  //   fromEvent(button, "click")
+  //     .pipe(
+  //       scan((acc, evt) => acc + 1, 0),
+  //       concatMap(id =>
+  //         api.getComment(id).pipe(
+  //           catchError((err, src$) => {
+  //             console.log("CATCH!");
+  //             return src$;
+  //           })
+  //         )
+  //       ),
+  //       map(JSON.stringify),
+  //       tap(console.log)
+  //     )
+  //     .subscribe(displayLog, err => console.log("ERROR ==> ", err.message));
+
   fromEvent(button, "click")
     .pipe(
       scan((acc, evt) => acc + 1, 0),
-      concatMap(id => api.getComment(id)),
+      concatMap(id => api.getComment(id).pipe(retry(3))),
       map(JSON.stringify),
       tap(console.log)
     )
-    .subscribe(displayLog);
+    .subscribe(displayLog, err => console.log("ERROR ==> ", err.message));
 
   /** end coding */
 };
