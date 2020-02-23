fe808375ca5aa3a12f82214b9f00824686adfcf6
Aleix Riba <aleix.riba@cabsa.es>
2020-02-23 15:53:49 +0100

    start class

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,7 +1,7 @@
 import { updateDisplay, displayLog } from "./utils";
 import { api } from "./api";
 import { fromEvent } from "rxjs";
-import { map, scan, tap, mergeMap, switchMap } from "rxjs/operators";
+import { map, scan, tap, concatMap } from "rxjs/operators";
 
 export default () => {
   /** start coding */
@@ -9,28 +9,10 @@ export default () => {
   const button = document.getElementById("btn");
 
   /** get comments on button click */
-  //   fromEvent(button, "click")
-  //     .pipe(
-  //       scan((acc, evt) => acc + 1, 0),
-  //       mergeMap(id => api.getComment(id)),
-  //       map(JSON.stringify),
-  //       tap(console.log)
-  //     )
-  //     .subscribe(displayLog);
-
-  //     fromEvent(button, "click")
-  //     .pipe(
-  //       scan((acc, evt) => acc + 1, 0),
-  //       switchMap(id => api.getComment(id)),
-  //       map(JSON.stringify),
-  //       tap(console.log)
-  //     )
-  //     .subscribe(displayLog);
-
   fromEvent(button, "click")
     .pipe(
       scan((acc, evt) => acc + 1, 0),
-      concatMap(id => api.getComment(id)),
+      concatMap(page => api.getCommentsList(page)),
       map(JSON.stringify),
       tap(console.log)
     )
