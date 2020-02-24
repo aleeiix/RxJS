fa3e85f02a5d68dec9a618343c0bd5a5593ce0c4
Aleix Riba <aleix.riba@cabsa.es>
2020-02-24 16:46:06 +0100

    finish class

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,5 +1,6 @@
 import { game$ } from "./tic-tac-toe/game";
 import { drawGame, writeMessage } from "./tic-tac-toe/draw";
+import { timer } from "rxjs";
 
 export default () => {
   /** start coding */
@@ -7,6 +8,14 @@ export default () => {
   game$.subscribe(
     gameState => {
       drawGame(gameState);
+      console.log(gameState);
+      if (gameState.winner) {
+        timer(500).subscribe(() =>
+          writeMessage(gameState.winner === 1 ? "You Win" : "You Lose")
+        );
+      } else if (gameState.finished) {
+        timer(500).subscribe(() => writeMessage("Draws"));
+      }
     },
     err => console.log("error: ", err),
     data => console.log("COMPLETE")
---
+++ src/tic-tac-toe/userMove.js
@@ -0,0 +1,20 @@
+import { canvas, CELL_SIZE } from "./draw";
+import { fromEvent } from "rxjs";
+import { map, withLatestFrom, tap, filter } from "rxjs/operators";
+import { gameState$ } from "./gameState";
+
+const click$ = fromEvent(canvas, "click").pipe(
+  map(val => {
+    return {
+      x: Math.floor(val.offsetX / CELL_SIZE),
+      y: Math.floor(val.offsetY / CELL_SIZE)
+    };
+  })
+);
+
+export const userMove$ = click$.pipe(
+  withLatestFrom(gameState$),
+  filter(([click, state]) => state.nextPlayer === 1),
+  filter(([click, state]) => state.board[click.y][click.x] === 0),
+  map(([click, state]) => click)
+);
